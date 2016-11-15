6f32bd1e803034808a7943252211847f9d53791c
Alireza Andersen <alireza.andersen@gmail.com>
2016-11-15 14:19:41 -0700

    Roman Numerals

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,12 +1,30 @@
-var decToNum = {   1: "I",   4:"IV",   5: "V",   9:"IX",
-				  10: "X",  40:"XL",  50: "L",  90:"XC",
-				 100: "C", 400:"CD", 500: "D", 900:"CM",
-				1000: "M",
-				};
+  var intToRomNum = {   1: "I",   4:"IV",   5: "V",   9:"IX",
+            10: "X",  40:"XL",  50: "L",  90:"XC",
+            100: "C", 400:"CD", 500: "D", 900:"CM",
+            1000: "M",
+  };
 
-var toRoman = function() {
+  var toRoman = function(remainer) {
+    /* Convert a decimal number to a roman numeral */
 
-};
+    var appendNumerals = function(total, decimal) {
+      /* Appends as many roman numerals as possible */
+      while (remainer >= decimal) {
+        remainer -= decimal;
+        total += intToRomNum[decimal];
+      }
+      return total;
+    };
+
+    var decimalNumbers = Object.keys(intToRomNum).sort(reverseNumeric);
+      return decimalNumbers.reduce( appendNumerals, '');
+  };
+
+  var reverseNumeric = function(a, b){
+    /* Reverse sort a numeric array */
+    return b - a;
+  };
 
 
-module.exports = toRoman; 
+
+module.exports = toRoman;
