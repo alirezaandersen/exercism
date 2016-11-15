4115147d56f1e7553443443cdaa2432416a3340d
Alireza Andersen <alireza.andersen@gmail.com>
2016-11-15 09:05:53 -0700

    Add Scrable

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -3,22 +3,15 @@ var Acronyms = function(){
 };
 
 Acronyms.prototype.parse = function(phrase) {
-  //  capitalizeEachWord(phrase);
+  return phrase.split(/\W+/)
+          .map(function(word) {
+              firstChar = word.charAt(0).toUpperCase();
+              if(word.match(/^[A-Z]+$/)) {
+                  return firstChar;
+              }
+              return firstChar + word.slice(1).replace(/[^A-Z]/g,"");
+          })
+          .join('');
+        };
 
- // var removeWhiteSpace = phrase.match(/\b(\w)/g).join();
- var removeWhiteSpace = capitalizeEachWord(phrase).match(/\b(\w)/g).join();
- console.log(removeWhiteSpace);
- var removeCommas = removeWhiteSpace.replace(new RegExp(/[^a-zA-Z:]/g),'');
- console.log(removeCommas);
- return removeCommas.toUpperCase();
-};
-
-  var capitalizeEachWord = function(phrase) {
-    // return phrase.replace(/\b(\w)/g, function(txt) {
-    return phrase.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toUpperCase() ;
-    });
-  };
-
-
-module.exports = new Acronyms;
+module.exports = new Acronyms();
