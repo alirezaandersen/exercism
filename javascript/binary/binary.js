d20f1bb7c30978536a2169abd323c3a6bd8965d9
Alireza Andersen <alireza.andersen@gmail.com>
2016-11-17 13:54:33 -0700

    binary.js

---
+++ javascript/binary/binary.js
@@ -0,0 +1,18 @@
+var Binary = function(binString){
+  this.binString = binString;
+};
+
+
+Binary.prototype.toDecimal = function(){
+
+  if (this.binString.match(/[^10]/))
+  return 0;
+
+  return this.binString.split('').reverse().reduce(
+		function(total, bit, power) {
+			return total + bit * Math.pow(2, power);
+		}, 0
+	);
+};
+
+module.exports = Binary;
