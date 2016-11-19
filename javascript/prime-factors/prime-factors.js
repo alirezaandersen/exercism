d5d967c5f4e443fc66c83c4de4cfe0b60e2a805f
Alireza Andersen <alireza.andersen@gmail.com>
2016-11-19 08:33:54 -0700

    prime-factor.js

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,9 +1,18 @@
-var primeFactors = function(){
+function primeFactors(remainder) {
+	/* Finds all prime factors of a number */
+	// Finds factors smallest to largest adding to the list of factors
+	var factor = 2;
+	var factors = [];
+	while(Math.pow(factor, 2) <= remainder) {
+		while(remainder % factor === 0) {
+			factors.push(factor);
+			remainder /= factor;
+		}
+		factor += factor == 2 ? 1 : 2;
+	}
+	if(remainder != 1)
+		factors.push(remainder);
+	return factors;
+}
 
-};
-
-primeFactors.prototype.toEqual = function(){
-
-};
-
-module.exports = primeFactors;
+module.exports = {for: primeFactors};
