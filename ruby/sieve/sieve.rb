0e9ff9fe33221d6961f6c16e96a3d77bba110772
Alireza Andersen <alireza.andersen@gmail.com>
2016-11-20 11:34:32 -0700

    Sieve.rb

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,11 @@
+class Sieve
+	attr_reader :primes
+
+  def initialize(len)
+    @primes = [nil, nil, *2..len]
+    (2..len**0.5).each do |n|
+      (n**2..len).step(n) { |m| @primes[m] = nil } if @primes[n]
+    end
+    @primes.compact!
+  end
+end
