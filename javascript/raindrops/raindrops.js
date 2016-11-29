5da1a2839febe655b4a3b856d2e3d1d881a7ecfc
Alireza Andersen <alireza.andersen@gmail.com>
2016-11-28 19:26:55 -0700

    raindrops.js

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,20 @@
+var Raindrops = function() {};
+
+Raindrops.prototype.convert = function(number) {
+    var out = "";
+    if (number % 3 === 0) {
+        out += "Pling";
+    }
+    if (number % 5 === 0) {
+        out += "Plang";
+    }
+    if (number % 7 === 0) {
+        out += "Plong";
+    }
+    if (out.length === 0) {
+        out = number.toString();
+    }
+    return out;
+};
+
+module.exports = Raindrops;
