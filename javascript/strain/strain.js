6e56d455a5260b37e3b525271ec806c6fcb2d5ac
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-04 07:41:01 -0700

    Strain based on count increase

--- javascript/strain/strain.js
+++ javascript/strain/strain.js
@@ -1,15 +1,19 @@
-var Strain = function() {};
+function keep(items, func) {
+	/* Selects items for which a function is True */
+	var kept = [];
+	for(var i = 0; i < items.length; i++)
+		if( func( items[i] ) )
+			kept.push( items[i] );
+	return kept;
+}
 
-Strain.keep = function(subject, fn) {
-  return subject.filter(fn);
+function discard(items, func) {
+	/* Selects items for which a function is False */
+	var not_func = function(item) { return !func(item); };
+	return keep(items, not_func);
+}
+
+module.exports = {
+  keep: keep,
+  discard: discard,
 };
-
-Strain.discard = function(subject, fn) {
-  var out = [];
-  subject.forEach((item)  {
-    return if((!fn(item)) { out.push(item)); }
-  });
-  return out;
-};
-
-module.exports = Strain;
