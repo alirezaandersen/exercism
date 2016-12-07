c58e99b969a756036b1162f5340e13d08e4916f0
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-07 14:35:04 -0700

    gradeschool

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,4 +1,22 @@
-class GradeSchool
 
+class School
+  def initialize
+    @roster = Hash.new { |h,k| h[k] = [] }
+  end
 
-end 
+  def students(grade)
+    @roster[grade]
+  end
+
+  def students_by_grade
+    @roster.keys.sort.map { |i| {grade: i, students: @roster[i]} }
+  end
+
+  def add(name, grade)
+    @roster[grade].push(name).sort!
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
