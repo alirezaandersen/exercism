be7690ecc267e863df8b62206dfa8db65f33ef22
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-13 23:42:57 -0700

    No more Bottles of Beer on the wall

---
+++ ruby/beer-song/beer_song.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class BeerSong
+
+  def lyrics
+    verses(99, 0)
+  end
+
+  def verses(increase_count, decrease_count)
+    increase_count.downto(decrease_count).map { |i| verse(i) }.join("\n")
+  end
+
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 1
+      "%s bottle of beer on the wall, %s bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n" % [number, number]
+    when 2
+      "%s bottles of beer on the wall, %s bottles of beer.\nTake one down and pass it around, %s bottle of beer on the wall.\n" % [number, number, number - 1]
+    else
+      "%s bottles of beer on the wall, %s bottles of beer.\nTake one down and pass it around, %s bottles of beer on the wall.\n" % [number, number, number - 1]
+    end
+  end
+
+end
+
+module BookKeeping
+  VERSION = 2
+end
