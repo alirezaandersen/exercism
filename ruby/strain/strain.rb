d37b5795d0e09cdfd99b7624831124f8f7d4ae8e
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-13 21:40:19 -0700

    Strain.rb

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -2,16 +2,16 @@ require 'pry'
 
 class Array
 
-def keep
-  kept = []
-  each {|e| kept << e if yield(e) }
-  kept
-end
+  def keep
+    kept = []
+    each {|e| kept << e if yield(e) }
+    kept
+  end
 
-def discard
-  kept = []
-  each {|e| kept << e unless yield(e) }
-  kept
-end
-
+  def discard
+    kept = []
+    each {|e| kept << e unless yield(e) }
+    kept
+  end
+  
 end
