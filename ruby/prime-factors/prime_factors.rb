13eb28fc2f2f1828f34f781dd2f74adf6db60fe2
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-13 20:40:07 -0700

    prime_factor.rb

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,4 +1,12 @@
+require 'prime'
+
 class PrimeFactors
 
+  def self.for(n)
+   divisors = Prime.prime_division(n).flat_map do |factor, power|
+      [factor] * power
+    end
+    divisors
+  end
 
 end
