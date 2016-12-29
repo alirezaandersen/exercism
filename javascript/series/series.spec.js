71bbe599e6d8393d6826193c4d63ba289bdebde9
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-28 17:37:21 -0700

    Series.js

--- javascript/series/series.spec.js
+++ javascript/series/series.spec.js
@@ -31,12 +31,12 @@ describe('Series', function () {
       .toEqual([[9, 8], [8, 2], [2, 7], [7, 3], [3, 4], [4, 6], [6, 3]]);
   });
 
-  xit('can slice by 3', function () {
+  it('can slice by 3', function () {
     expect(new Series('01234').slices(3))
       .toEqual([[0, 1, 2], [1, 2, 3], [2, 3, 4]]);
   });
 
-  xit('can slice by 3 with duplicate digits', function () {
+  it('can slice by 3 with duplicate digits', function () {
     expect(new Series('31001').slices(3))
       .toEqual([[3, 1, 0], [1, 0, 0], [0, 0, 1]]);
   });
