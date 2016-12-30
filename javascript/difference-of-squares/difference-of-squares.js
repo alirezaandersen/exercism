f3fdc3847f4d88ddbd46bd312cfa5605afb6df2e
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-29 23:47:11 -0700

    difference.js

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -1,5 +1,8 @@
 var Squares = function(num) {
-
+  /* Calculates sums of difference functions */
+	// Based on triangle numbers and square pyramid numbers
+	this.squareOfSums = (Math.pow(num, 2) * Math.pow(num + 1, 2)) / 4;
+	
 };
 
 module.exports = Squares;
