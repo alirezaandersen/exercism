99a307b659f5a25f4b221c4bcd22ea758b4f69a6
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-01 23:21:53 -0700

    linked-list

--- javascript/linked-list/linked-list.spec.js
+++ javascript/linked-list/linked-list.spec.js
@@ -22,7 +22,7 @@ describe('LinkedList', function () {
     expect(list.shift()).toBe(20);
     expect(list.shift()).toBe(10);
   });
-  xit('unshift/pop', function () {
+  it('unshift/pop', function () {
     var list = new LinkedList();
     list.unshift(10);
     list.unshift(20);
