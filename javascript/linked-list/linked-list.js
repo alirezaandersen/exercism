6f709179e9e3a6915a41faf2a3a65ece9daecf45
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-01 23:11:21 -0700

    linked-list

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,5 +1,20 @@
 var LinkedList = function(){
+  /* A linked list */
+  this.head = null;
+  this.tail = null;
+  this.lenght = 0;
 
+};
+
+LinkedList.prototype.push = function(val) {
+  /* Adds a value to the end of the linked list */
+  this.tail = new Element(val, null, this.tail);
+  if(this.length >=1)
+    this.tail.prev.next = this.tail;
+    else {
+      this.head = this.tail;
+      this.length ++; 
+    }
 };
 
 module.exports = LinkedList;
