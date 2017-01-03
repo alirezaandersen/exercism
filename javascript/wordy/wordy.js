ca2898eca6f0986a634388420b69fce920d0e2ae
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-03 08:06:32 -0700

    wordy

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,5 +1,10 @@
 var WordProblem = function(){
-
+  /* Solves mathmatical word problems */
+  this.text = text;
+  this.operators = {
+      "plus" : '+'
+  };
+  this.form = new RegExp("^What is (-?\\d+(?: (?:" + Object.keys(this.operators).join("|") + ") -?\\d+)+)\\?$");
 };
 
 
