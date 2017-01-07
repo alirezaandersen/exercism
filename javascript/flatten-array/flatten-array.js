3d73284585ae5c36768c757ec6d49227dbb7e38a
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-06 22:58:36 -0700

    flatten-array

--- javascript/flatten-array/flatten-array.js
+++ javascript/flatten-array/flatten-array.js
@@ -1,5 +1,22 @@
-var Flattener = function(){
+var Flattener = function() {};
 
+Flattener.prototype.flatten = function(list) {
+    var flattener = function(list, result) {
+        list.forEach(function(value) {
+            if (typeof value !== 'object') {
+                result.push(value);
+                return;
+            }
+            // array case;
+            if (value && typeof value.length !== 'undefined') {
+                flattener(value, result);
+            }
+        });
+
+        return result;
+    };
+
+    return flattener(list, []);
 };
 
 module.exports = Flattener;
