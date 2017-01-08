334329e618f4b1f1b565bdcabec3b295957c52aa
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-07 17:12:42 -0700

    Hexadecimal.js

--- javascript/hexadecimal/hexadecimal.js
+++ javascript/hexadecimal/hexadecimal.js
@@ -1,5 +1,16 @@
-var Hexadecimal = function(){
-
+var Hexadecimal = function(hex_digits) {
+	/* Hexadeciaml number class */
+	this.hex_digits = hex_digits;
 };
 
-module.exports = Hexadecimal; 
+function hex_digit_value(digit) {
+	/* Converts a hexadecimal digit to integer or NaN*/
+	if(digit.match(/[0-9]/))
+		return parseInt(digit);
+	else if(digit.match(/[a-f]/))
+		return digit.charCodeAt(0) - 'a'.charCodeAt(0) + 10;
+	else
+		return NaN;
+}
+
+module.exports = Hexadecimal;
