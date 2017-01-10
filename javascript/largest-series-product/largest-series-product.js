6a69ac2f535204feda61dab234aee2976f20017c
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-09 23:40:25 -0700

    largest series product.js

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,18 +1,24 @@
 var Series = function(digits) {
-	/* A series of digits */
-	this.digits = digits.split('').map(function(e) {return parseInt(e);
-  });
+    /* A series of digits */
+    this.digits = digits.split('').map(function(e) {
+        return parseInt(e);
+    });
 };
 
 Series.prototype.slices = function(size) {
-	/* Slices digits into groups */
-	if(this.digits.length < size)
-		throw new Error("Slice size is too big.");
-	var slices = [];
-	for(var i = 0; i + size <= this.digits.length; i++ ) {
-		slices.push(this.digits.slice(i, i + size));
-	}
-	return slices;
+    /* Slices digits into groups */
+    if (this.digits.length < size)
+        throw new Error("Slice size is too big.");
+    var slices = [];
+    for (var i = 0; i + size <= this.digits.length; i++) {
+        slices.push(this.digits.slice(i, i + size));
+    }
+    return slices;
+};
+
+Series.prototype.laregestProduct = function(size) {
+    /* The largest group when digits of slices are multipled together */
+    return Math.max.apply(null, this.slices(size).map(multiply));
 };
 
 module.exports = Series;
