608b5b4313b708320fb305b58255d59e721a7347
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-10 00:00:17 -0700

    largest series product.js

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,32 +1,31 @@
 var Series = function(digits) {
-    /* A series of digits */
-    this.digits = digits.split('').map(function(e) {
-        return parseInt(e);
-    });
+	/* A series of digits */
+  if (!digits.match(/^\d*$/g)) { throw new Error('Invalid input.'); }
+	this.digits = digits.split('').map(function(e) {return parseInt(e);});
 };
 
 Series.prototype.slices = function(size) {
-    /* Slices digits into groups */
-    if (this.digits.length < size)
-        throw new Error("Slice size is too big.");
-    var slices = [];
-    for (var i = 0; i + size <= this.digits.length; i++) {
-        slices.push(this.digits.slice(i, i + size));
-    }
-    return slices;
+	/* Slices digits into groups */
+	if(this.digits.length < size)
+		throw new Error("Slice size is too big.");
+	var slices = [];
+	for(var i = 0; i + size <= this.digits.length; i++ ) {
+		slices.push(this.digits.slice(i, i + size));
+	}
+	return slices;
 };
 
-Series.prototype.laregestProduct = function(size) {
-    /* The largest group when digits of slices are multipled together */
-    return Math.max.apply(null, this.slices(size).map(multiply));
+Series.prototype.largestProduct = function(size) {
+	/* The largest group when digits of slices are multipled together */
+  if (size < 0) {throw new Error('Invalid input.');}
+	return Math.max.apply(null, this.slices(size).map(multiply));
 };
 
 function multiply(numbers) {
-    /* Multiples all elements of an array together */
-    return numbers.reduce(function(total, element) {
-        return total * element;
-    }, 1);
+	/* Multiples all elements of an array together */
+	return numbers.reduce(function(total, element) {
+		return total * element;
+	}, 1);
 }
-
 
 module.exports = Series;
