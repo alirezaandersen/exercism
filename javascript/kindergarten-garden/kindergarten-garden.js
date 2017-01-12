1a7bfa063cdeaf2beee6943ece3bd446e1ba77bc
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-11 22:54:06 -0700

    kindergarten garden

--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -1,7 +1,12 @@
 var DEFAULT_STUDENTS = [ 'Alice', 'Bob', 'Charlie', 'David', 'Eve',
 		'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry'];
-    
+
 var Garden = function(garden, students){
+  /* A Kindergarden plant garden */
+};
+
+function StudentsGarden(garden, position) {
+	/* The plants owned by an individual student */
 
 };
 
