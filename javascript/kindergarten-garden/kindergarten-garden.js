1ca751d39e8d6fea3f022d1c94ba6bbc37779db0
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-11 22:52:32 -0700

    kindergarten garden

--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -1,3 +1,6 @@
+var DEFAULT_STUDENTS = [ 'Alice', 'Bob', 'Charlie', 'David', 'Eve',
+		'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry'];
+    
 var Garden = function(garden, students){
 
 };
