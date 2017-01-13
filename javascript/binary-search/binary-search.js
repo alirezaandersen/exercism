0e2a74c7d43f284d07cdca4dc4d21b7c4f7891c1
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-12 23:55:18 -0700

    binarytree.js

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -2,8 +2,11 @@ var BinaryTree = function(data){
   /* Tree Data Structure */
   this.data = data;
   this.left = null;
-  this.right = null; 
+  this.right = null;
 };
 
+BinaryTree.prototype.insert = function(data) {
+	/* Adds a value to the tree */
+};
 
 module.exports = BinaryTree;
