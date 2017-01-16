762422b440876e5d0e8aed2db9a672197b6c00c3
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-15 22:20:08 -0700

    matrix.js

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,5 +1,8 @@
 var Matrix = function(matrix){
   /* A 2d matrix class */
+  this.rows = matrix.split("\n").map( function(row) {
+		return row.split(" ").map(function(e) { return  parseInt(e); });
+	});
 };
 
 module.exports = Matrix;
