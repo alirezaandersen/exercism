45ebbe72e7a4b3e35155601ab8d1ac419b632f6a
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-15 22:22:08 -0700

    matrix.js

--- javascript/matrix/matrix.spec.js
+++ javascript/matrix/matrix.spec.js
@@ -10,7 +10,7 @@ describe('Matrix', function () {
     expect(new Matrix('9 8 7\n19 18 17').rows[1]).toEqual([19, 18, 17]);
   });
 
-  xit('can extract a column', function () {
+  it('can extract a column', function () {
     expect(new Matrix('89 1903 3\n18 3 1\n9 4 800').columns[1])
       .toEqual([1903, 3, 4]);
   });
