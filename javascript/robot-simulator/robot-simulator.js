9830e99a376dbbf36d4c46b482a32b1603707aa5
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-17 22:17:19 -0700

    robot-simulator.js

--- javascript/robot-simulator/robot-simulator.js
+++ javascript/robot-simulator/robot-simulator.js
@@ -3,6 +3,7 @@ var Robot = function() {};
 
 // Directions the robot can face and associated vectors
 var directions = [ 'north', 'east', 'south', 'west' ];
+var vectors = [[0, 1], [1, 0], [0, -1], [-1, 0]];
 
 
 module.exports = Robot;
