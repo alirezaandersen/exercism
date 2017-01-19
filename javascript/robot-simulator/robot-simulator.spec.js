79befc74000061f23e86488bf08e2ff58e9122df
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-18 22:03:57 -0700

    robot-simulator.js

--- javascript/robot-simulator/robot-simulator.spec.js
+++ javascript/robot-simulator/robot-simulator.spec.js
@@ -69,7 +69,7 @@ describe('Robot', function() {
     expect(robot.bearing).toEqual('south');
   });
 
-  xit('robot coordinates', function() {
+  it('robot coordinates', function() {
     robot.at(3, 0);
     expect(robot.coordinates).toEqual([3,0]);
   });
