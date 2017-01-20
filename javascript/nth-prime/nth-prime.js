6c729e3b9739fbabb79e2909c8ed39b6672187e5
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-20 16:16:17 -0700

    Nth Prime.js

--- javascript/nth-prime/nth-prime.js
+++ javascript/nth-prime/nth-prime.js
@@ -5,7 +5,12 @@ function find_nth_prime(n) {
 
   var primes;
 	l: for (primes = [2], i = 3, root; primes.length < n; i += 2) {
-  }
+    for (root = Math.sqrt(i), j = 0; primes[j] <= root; j++) {
+			if (i%primes[j] === 0) continue l;
+		}
+		primes.push(i);
+	}
+	return primes [n - 1];
 }
 
 
