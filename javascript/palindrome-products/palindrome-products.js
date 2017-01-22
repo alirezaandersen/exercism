2d48826336c13774204fe023f00f6107d28edd78
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-22 13:10:51 -0700

    palindormes

--- javascript/palindrome-products/palindrome-products.js
+++ javascript/palindrome-products/palindrome-products.js
@@ -1,5 +1,7 @@
 var Palindromes = function(args){
-
+  /* Pairs of numbers that when multipied are palindromes */
+	this.maxFactors = args['maxFactor'];
+	this.minFactor = args['minFactor'] || 1;
 };
 
-module.exports = Palindromes; 
+module.exports = Palindromes;
