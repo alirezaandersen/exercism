fd841f9f7ee318cf24d20e0e42f6b7f1a1536686
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-22 13:11:40 -0700

    palindormes

--- javascript/palindrome-products/palindrome-products.js
+++ javascript/palindrome-products/palindrome-products.js
@@ -4,6 +4,12 @@ var Palindromes = function(args){
 	this.minFactor = args['minFactor'] || 1;
 };
 
+Palindromes.prototype.largest = function() {
+	/* The largest product generated */
+	var keys = Object.keys(this.palindromes);
+	var max = Math.max.apply(null, keys);
+	return {value: max, factors: this.palindromes[max]};
+};
 
 function is_palindrome(str) {
 	for(var i = 0; i < (str.length / 2); i++) {
