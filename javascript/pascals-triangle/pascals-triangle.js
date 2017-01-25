2d2dbf8722eeabe9e217b07903f35ebfde6c68c5
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-24 23:23:36 -0700

    pascals-triangle.js

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,24 +1,24 @@
-var Triangle = function(nRows){
-  /* Builds Pascals Triangle */
-  this.row = [];
-  var row = [1];
-  for(var i = 0 ; i < nRows; i++){
-    this.row.push(row);
-    this.lastRow = row;
-    row = nextRow(row);
-  }
+var Triangle = function(nRows) {
+	/* Builds Pascals Triangle */
+	this.rows = [];
+	var row = [1];
+	for(var i = 0; i < nRows; i++ ) {
+		this.rows.push(row);
+		this.lastRow = row;
+		row = nextRow(row);
+	}
 };
 
-  function nextRow(currentRow){
-    /* Builds the next row of Pascals Triangle*/
-    var row = [];
-    var prev = 0;
-    for(var j =0 ; j < currentRow.length; j++){
-      row.push(currentRow[j] + prev);
-      prev = currentRow[j];
-    }
-    row.push(prev);
-    return row;
-  }
+function nextRow(currentRow) {
+	/* Builds the next row of Pascals Triangle */
+	var row = [];
+	var prev = 0;
+	for(var j = 0; j < currentRow.length; j++) {
+		row.push(currentRow[j] + prev);
+		prev = currentRow[j];
+	}
+	row.push(prev);
+	return row;
+}
 
 module.exports = Triangle;
