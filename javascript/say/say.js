429dd0288da44f0d6b129cb652f26c3a4112a1be
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-26 22:28:12 -0700

    say.js

--- javascript/say/say.js
+++ javascript/say/say.js
@@ -1,8 +1,16 @@
 function inEnglish(num) {
-  /* A number in english */
-  	if(0 > num || num >= 1000000000000)
-  		throw new Error('Number must be between 0 and 999,999,999,999.');
+	/* A number in english */
+	if(0 > num || num >= 1000000000000)
+		throw new Error('Number must be between 0 and 999,999,999,999.');
 
+	if(num == 0)
+		return "zero";
+
+	var digit_groups = [];
+	while(0 < num) {
+		digit_groups.push(num % 1000);
+		num = Math.floor(num / 1000);
+	}
 }
 
 
