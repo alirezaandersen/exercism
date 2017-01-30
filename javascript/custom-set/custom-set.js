797809e9ece76732a4328aba736102f03c4de63e
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-29 21:39:01 -0700

    custom-set.js

--- javascript/custom-set/custom-set.js
+++ javascript/custom-set/custom-set.js
@@ -1,5 +1,16 @@
-var CustomSet = function(){
 
+var CustomSet = function(items) {
+	/* A set object */
+	this.items = [];
+	for(var i = 0; i < (items ? items.length : 0); i++)
+		this.put(items[i]);
+};
+
+
+
+CustomSet.prototype.subset = function(set_b) {
+	/* All elements of one set are contained in this set */
+	return set_b.toList().every(function(element) {return this.member(element)}, this);
 };
 
 module.exports = CustomSet;
