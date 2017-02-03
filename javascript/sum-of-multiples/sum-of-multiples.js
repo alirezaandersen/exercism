16cf3eaa856e6aa75f3174028cb163781178c148
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-02 19:46:44 -0700

    sum of multiples

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -1,5 +1,10 @@
-function to(num){
-
+function to(num) {
+	/* Sum of numbers that are divisible by at least one of the multiples */
+	return Array.apply(null, Array(num)).reduce(function(total, _, n) {
+		if(this.some(function(mul) { return n % mul === 0; }))
+			return total + n;
+		return total;
+	}.bind(this.factors), 0);
 }
 
 module.exports = function(factors) {
