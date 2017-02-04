61c14d99a6ab9caeba69d1839d465e77a837ec76
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-04 00:39:36 -0700

    queens

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -10,7 +10,13 @@ Queens.prototype.toString = function() {
 	/* String representation of the chess board */
 	return Array.apply(null, Array(8)).map(function(_, i) {
 		return Array.apply(null, Array(8)).map(function(_, j) {
-
+      if(this.white[0] == i && this.white[1] == j)
+				return "W";
+			if(this.black[0] == i && this.black[1] == j)
+				return "B";
+			return "_";
+		}, this).join(" ") + "\n";
+	}, this).join("");
 };
 
 module.exports = Queens;
