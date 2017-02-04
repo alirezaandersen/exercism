b7cd36c70a5371280d686ae02c0d2ed65c5a16cb
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-04 00:39:25 -0700

    queens

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -4,10 +4,13 @@ var Queens = function(){
 	this.black = (args && args.black) || [7, 3];
 	if(this.white === this.black)
 		throw "Queens cannot share the same space";
-}
+};
 
 Queens.prototype.toString = function() {
 	/* String representation of the chess board */
+	return Array.apply(null, Array(8)).map(function(_, i) {
+		return Array.apply(null, Array(8)).map(function(_, j) {
+
 };
 
 module.exports = Queens;
