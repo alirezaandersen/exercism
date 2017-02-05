fcfd8298c88a111ac1dfd7c3c609d18ed616f302
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-05 12:11:07 -0700

    saddle-points

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,5 +1,11 @@
-var Matrix = function(){
-
+var Matrix = function(matrix){
+  /* A 2nd Matrix Class*/
+  this.rows = matrix.split("\n").map(function(row){
+    return row.split(" ").map(function(e){
+      return pareInt(e);
+    });
+  });
+  
 };
 
 module.exports = Matrix;
