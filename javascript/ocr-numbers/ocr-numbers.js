36f9d06d3ee7f743a4696bc84723e398ab30fc6e
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-08 01:50:18 -0700

    ocr-numbers

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -2,4 +2,8 @@ function convert(OCR_string) {
 	/* Convert a line representation of digits into a string of digits */
 }
 
+function lines_to_characters(line) {
+	/* Divides a set of lines into characters */
+}
+
 module.exports = {convert: convert};
