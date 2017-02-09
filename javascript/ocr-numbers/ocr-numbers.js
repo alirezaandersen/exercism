ceae288f4f684814c10ca63c285fc667bcbf7438
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-09 09:58:40 -0700

    ocr-numbers

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,6 +1,19 @@
 function convert(OCR_string) {
 	/* Convert a line representation of digits into a string of digits */
+  OCR_lines = OCR_string.split("\n");
+OCR_characters = [];
+for(var line = 0; line < OCR_lines.length; line += 4) {
+  OCR_characters.push(lines_to_characters(OCR_lines.slice(line, line + 4)));
 }
+
+return OCR_characters.map(function(row) {
+  return row.map(function(character) {
+    var digit = OCR_digits.indexOf(character);
+    return  digit != -1 ? digit.toString() : "?";
+  }).join("");
+}).join(",");
+}
+
 
 function lines_to_characters(line) {
 	/* Divides a set of lines into characters */
