8075538b0a7f56466b5f27daa17a5d302247ef7a
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-12 22:27:59 -0700

    meetup.js

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -3,6 +3,8 @@ var weekDays = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(
 var Meetup = function(year, month, day, nth){
   /* Finds a meetup date */
 
+var intDay = weekDays.indexOf(day);
+
 };
 
 
