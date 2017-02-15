46a7acdd1e7dec9ba35a63f8898808ec359870d7
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-15 08:27:09 -0700

    Two Buckets

--- javascript/two-bucket/two-bucket.js
+++ javascript/two-bucket/two-bucket.js
@@ -1,5 +1,9 @@
 var TwoBucket = function(sizeOne, sizeTwo, goal, starterBucket) {
 	/* Runs the two bucket problem */
+  this.goalBucketSize = (starterBucket == "one" ? sizeOne : sizeTwo);
+	this.otherBucketSize = (starterBucket == "one" ? sizeTwo : sizeOne);
+	this.goalBucket = starterBucket;
+	this.goal = goal;
 };
 
 TwoBucket.prototype.moves = function() {
