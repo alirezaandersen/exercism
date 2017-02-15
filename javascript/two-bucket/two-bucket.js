59022facc2349e5647430feb2b79fea44c49ea5d
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-15 08:26:20 -0700

    Two Buckets

--- javascript/two-bucket/two-bucket.js
+++ javascript/two-bucket/two-bucket.js
@@ -1,6 +1,9 @@
 var TwoBucket = function(sizeOne, sizeTwo, goal, starterBucket) {
 	/* Runs the two bucket problem */
+};
 
+TwoBucket.prototype.moves = function() {
+	/* Fills buckets until one contains a certain amount of water */
 };
 
 module.exports = TwoBucket;
