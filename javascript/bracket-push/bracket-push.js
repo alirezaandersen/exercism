48015d41bdae8365c9acfa6f7681316691329350
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-14 22:51:51 -0700

    bracket-push

--- javascript/bracket-push/bracket-push.js
+++ javascript/bracket-push/bracket-push.js
@@ -4,6 +4,16 @@ function bracket(brackets) {
 	/* checks that bracket syntax is correct */
   var stack = [];
   for( var i = 0; i < brackets.length; i++) {
+    var letter = brackets[i];
+    if( bracket_pairs.hasOwnProperty(letter) ) {
+      stack.push(bracket_pairs[letter]);
+    } else if( stack.length !== 0 && stack[stack.length - 1] == letter) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
 
 }
 
