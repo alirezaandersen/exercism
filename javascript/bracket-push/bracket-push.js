e79d78f9a6d763490edbb8c4a807e2dedaa0dbe1
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-14 22:51:44 -0700

    bracket-push

--- javascript/bracket-push/bracket-push.js
+++ javascript/bracket-push/bracket-push.js
@@ -3,6 +3,7 @@ var bracket_pairs = {"{": "}", "[":"]", "(":")"};
 function bracket(brackets) {
 	/* checks that bracket syntax is correct */
   var stack = [];
+  for( var i = 0; i < brackets.length; i++) {
 
 }
 
