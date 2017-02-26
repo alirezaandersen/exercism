7a6abdcf86893921fb17a4c2b29dc1b500c639c5
Alireza Andersen <alireza.andersen@gmail.com>
2017-02-25 17:04:15 -0700

    crypto

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -10,4 +10,9 @@ class Crypto
   def size
     Math.sqrt(normalize_plaintext.length).ceil
   end
+
+  def plaintext_segments
+    normalize_plaintext.chars.each_slice(size).collect { |i| i.join }.to_a
+  end
+
 end
