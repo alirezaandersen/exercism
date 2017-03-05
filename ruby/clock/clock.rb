205fea81f00c11730136584893bdd4bb78c46afe
Alireza Andersen <alireza.andersen@gmail.com>
2017-03-04 21:27:25 -0700

    clock.rb

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -7,7 +7,12 @@ class Clock
   def initialize(hour, minute)
       @minutes = hour * 60 + minute
   end
-    
+
+  def +(hour=0, minute)
+    @minutes += hour * 60 + minute
+    self
+  end
+
 end
 
 
