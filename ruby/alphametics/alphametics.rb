704dbf4a1affd3103e90a625c5e5de5f9e019da9
Alireza Andersen <alireza.andersen@gmail.com>
2017-03-14 08:30:40 -0600

    alphametics.rb

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,4 +1,24 @@
 class Alphametics
 
+  def solve(input)
+    # Sub in the power operator.
+    input.gsub!(/ \^ /, '**')
+    distinct_letters = input.gsub(/[^A-Z]/, '').chars.uniq
+
+    # Get all the possible answer maps.
+    options = (0..9).to_a
+      .permutation(distinct_letters.length).to_a.map do |p|
+        p.zip(distinct_letters).each_with_object({}) { |(i,l), a| a[l] = i }
+      end
+
+    # have multiple valid answers
+    out = options.select do |o|
+      check = input.tr(o.keys.join, o.values.join)
+      # Numbers can't start with a 0.
+      next if check =~ /^0| 0\d/
+      o if eval(check)
+    end
+    out[0]
+  end
 
 end
