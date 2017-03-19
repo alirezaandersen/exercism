5d7bc9293d66c765d3fa6290062b984cd25c14f6
Alireza Andersen <alireza.andersen@gmail.com>
2017-03-18 23:42:20 -0600

    rail fence cipher

--- ruby/rail-fence-cipher/rail-fence-cipher.rb
+++ ruby/rail-fence-cipher/rail-fence-cipher.rb
@@ -1,5 +1,11 @@
 class RailFenceCipher
-VERSION = 1
+  VERSION = 1
+  def self.encode(message, rails)
+    return message if message.empty? || rails == 1
+    empty_fences = create_empty_fences_array(message, rails)
+    zig_zag_array = populate_fences_array(message, rails, empty_fences)
+    zig_zag_array.flatten.join
+  end
 
 
 end
