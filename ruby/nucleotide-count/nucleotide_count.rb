d8b1088e64b3749c84126172b0314254a4acf9f7
Alireza Andersen <alireza.andersen@gmail.com>
2017-04-05 20:46:17 -0600

    nucleotide_count.rb

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,6 +1,10 @@
 class Nucleotide
   attr_reader :acids
 
+  def initialize(acids)
+    @acids = acids
+  end
+
   def self.from_dna(strand)
     acids = strand.chars
     fail ArgumentError.new("Invalid DNA #{strand}") unless acids.all?(&validate)
