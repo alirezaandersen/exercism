86b63fb09af0129095322450b4de2a78113c74e2
Alireza Andersen <alireza.andersen@gmail.com>
2017-04-11 08:46:31 -0600

    hexadecimal

--- ruby/hexadecimal/hexdecimal.rb
+++ ruby/hexadecimal/hexdecimal.rb
@@ -6,4 +6,24 @@ class Hexadecimal
     @digits = decimal.reverse.chars
   end
 
+  def values
+    {
+      '0' => 0,
+      '1' => 1,
+      '2' => 2,
+      '3' => 3,
+      '4' => 4,
+      '5' => 5,
+      '6' => 6,
+      '7' => 7,
+      '8' => 8,
+      '9' => 9,
+      'a' => 10,
+      'b' => 11,
+      'c' => 12,
+      'd' => 13,
+      'e' => 14,
+      'f' => 15
+    }
+  end
 end
