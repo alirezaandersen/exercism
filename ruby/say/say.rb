84447991e7beaf032a35976c73cbcd77996c0e89
Alireza Andersen <alireza.andersen@gmail.com>
2017-04-17 21:09:25 -0600

    say.rb

--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -2,25 +2,29 @@ class Say
 
   attr_reader :value
 
-    def initialize(value)
-      @value = value
-    end
-
-    def in_english
-      guard_range
-
-      return 'zero' if value.zero?
-
-      chunks.join(' ').squeeze(' ').strip
-    end
-
-private
-
-def guard_range
-  unless in_range?
-    message = 'Number must be between 0 and 999,999,999,999 inclusive.'
-    fail ArgumentError.new(message)
+  def initialize(value)
+    @value = value
   end
-end
+
+  def in_english
+    guard_range
+
+    return 'zero' if value.zero?
+
+    chunks.join(' ').squeeze(' ').strip
+  end
+
+  private
+
+  def guard_range
+    unless in_range?
+      message = 'Number must be between 0 and 999,999,999,999 inclusive.'
+      fail ArgumentError.new(message)
+    end
+  end
+
+  def in_range?
+    value >= 0 && value < 10**12
+  end
 
 end
