29f235126d321c77ac51209ede33ac1c4e8bce65
Alireza Andersen <alireza.andersen@gmail.com>
2017-06-21 00:52:08 -0600

    say.rb

--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -9,57 +9,57 @@ class Chunk
     return '' if zero?
     s = say_hundreds
     s << say_double_digits
-  #
-  #   s << magnitude
+
+    s << magnitude
+  end
+
+  private
+
+  def say_hundreds
+    return '' unless hundreds?
+    "#{small_numbers[hundreds]} hundred"
+  end
+
+  def say_double_digits
+    return '' if double_digits.zero?
+    s = ' '
+    if double_digits < 20
+      s << small_numbers[double_digits]
+    else
+      s << decades[tens]
+      unless ones.zero?
+        s << '-'
+        s << small_numbers[ones]
+      end
+    end
+    s
+  end
+
+  def hundreds
+    @hundreds ||= value / 100
+  end
+
+  def double_digits
+    @double_digits ||= value % 100
+  end
+
+  # def tens
+  #   @tens ||= double_digits / 10
+  # end
+
+  # def ones
+  #   @ones ||= double_digits % 10
+  # end
+
+  # def hundreds?
+  #   !hundreds.zero?
   # end
   #
-  # private
-  #
-  # def say_hundreds
-  #   return '' unless hundreds?
-  #   "#{small_numbers[hundreds]} hundred"
+  # def zero?
+  #   value.zero?
   # end
   #
-  # def say_double_digits
-  #   return '' if double_digits.zero?
-  #   s = ' '
-  #   if double_digits < 20
-  #     s << small_numbers[double_digits]
-  #   else
-  #     s << decades[tens]
-  #     unless ones.zero?
-  #       s << '-'
-  #       s << small_numbers[ones]
-  #     end
-  #   end
-  #   s
-  # end
-  #
-  # def hundreds
-  #   @hundreds ||= value / 100
-  # end
-  #
-  # def double_digits
-  #   @double_digits ||= value % 100
-  # end
-  #
-  # # def tens
-  # #   @tens ||= double_digits / 10
-  # # end
-  #
-  # # def ones
-  # #   @ones ||= double_digits % 10
-  # # end
-  #
-  # # def hundreds?
-  # #   !hundreds.zero?
-  # # end
-  # #
-  # # def zero?
-  # #   value.zero?
-  # # end
-  # #
-  # # def magnitude?
+  # def magnitude?
   #   !!magnitude
   # end
 
