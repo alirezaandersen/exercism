18203d895386fa6566f2e502d2e430addc90616c
Alireza Andersen <alireza.andersen@gmail.com>
2017-09-16 14:38:53 -0600

    meetup.rb

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -1,4 +1,18 @@
 class Meetup
+  def initialize(month, year)
+    @cal = (Date.new(year, month, 1)..Date.new(year, month, -1))
+  end
 
-
-end 
+  def day(meet_up, schedule)
+    all_meet_ups = @cal.select { |date| date.send("#{meet_up}?") }
+    case schedule
+    when :first  then all_dows[0]
+    when :second then all_dows[1]
+    when :third  then all_dows[2]
+    when :fourth then all_dows[3]
+    when :last   then all_dows.last
+    when :teenth
+      all_dows.select { |d| d.mday > 12 && d.mday < 20 }.first
+    end
+  end
+end
