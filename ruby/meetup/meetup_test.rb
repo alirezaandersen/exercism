4a952355223a8d4c7cd010b83520c3654a89e7c8
Alireza Andersen <alireza.andersen@gmail.com>
2017-11-26 09:21:37 -0700

    meetup

--- ruby/meetup/meetup_test.rb
+++ ruby/meetup/meetup_test.rb
@@ -35,29 +35,29 @@ class MeetupTest < Minitest::Test
       Meetup.new(4, 2013).day(:tuesday, :teenth)
   end
 
-  # def test_tuesteenth_of_august_2013
-  #   skip
-  #   assert_equal Date.new(2013, 8, 13),
-  #     Meetup.new(8, 2013).day(:tuesday, :teenth)
-  # # end
-  #
-  # # def test_wednesteenth_of_january_2013
-  # #   skip
-  # #   assert_equal Date.new(2013, 1, 16),
-  # #     Meetup.new(1, 2013).day(:wednesday, :teenth)
-  # # end
-  #
+  def test_tuesteenth_of_august_2013
+    skip
+    assert_equal Date.new(2013, 8, 13),
+      Meetup.new(8, 2013).day(:tuesday, :teenth)
+  end
+
+  def test_wednesteenth_of_january_2013
+    skip
+    assert_equal Date.new(2013, 1, 16),
+      Meetup.new(1, 2013).day(:wednesday, :teenth)
+  end
+
   def test_wednesteenth_of_february_2013
     skip
     assert_equal Date.new(2013, 2, 13),
       Meetup.new(2, 2013).day(:wednesday, :teenth)
-  # end
+  end
 
-  # def test_wednesteenth_of_june_2013
-  #   skip
-  #   assert_equal Date.new(2013, 6, 19),
-  #     Meetup.new(6, 2013).day(:wednesday, :teenth)
-  # end
+  def test_wednesteenth_of_june_2013
+    skip
+    assert_equal Date.new(2013, 6, 19),
+      Meetup.new(6, 2013).day(:wednesday, :teenth)
+  end
 
   def test_thursteenth_of_may_2013
     skip
