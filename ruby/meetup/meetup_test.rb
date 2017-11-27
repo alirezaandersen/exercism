a7e631844ccae6bc0fc46327b4d0af9793498599
Alireza Andersen <alireza.andersen@gmail.com>
2017-11-27 11:24:57 -0700

    meetup

--- ruby/meetup/meetup_test.rb
+++ ruby/meetup/meetup_test.rb
@@ -209,7 +209,6 @@ class MeetupTest < Minitest::Test
   end
 
   def test_second_wednesday_of_august_2013
-    skip
     assert_equal Date.new(2013, 8, 14),
       Meetup.new(8, 2013).day(:wednesday, :second)
   end
