e39ad3f934d33a8d9f7eb667a98aadc54006b5cc
Alireza Andersen <alireza.andersen@gmail.com>
2017-11-29 08:32:16 -0700

    queen-attack

--- ruby/queen-attack/queen_attack_test.rb
+++ ruby/queen-attack/queen_attack_test.rb
@@ -85,7 +85,6 @@ class QueenAttackTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
