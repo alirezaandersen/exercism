3632af9bb3f5c4d29d5187097723a5c5cfd75090
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-08 15:59:25 -0700

    foodchain

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -18,5 +18,12 @@ module FoodChain
 I know an old lady who swallowed a fly.
 I don't know why she swallowed the fly. Perhaps she'll die.
       LINES
+    when 2
+      <<-LINES
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
 
 end
