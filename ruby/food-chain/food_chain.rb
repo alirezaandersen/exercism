da337e241d57fe35602b151356fa57daaf7c66e2
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-09 17:05:23 -0700

    foodchain

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -52,5 +52,16 @@ She swallowed the bird to catch the spider that wriggled and jiggled and tickled
 She swallowed the spider to catch the fly.
 I don't know why she swallowed the fly. Perhaps she'll die.
       LINES
+    when 6
+      <<-LINES
+I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
 
 end
