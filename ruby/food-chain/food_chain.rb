d65a65f80dc2ea8f0da65e36b0aeb66e6205b4e9
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-09 17:01:39 -0700

    foodchain

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -25,5 +25,13 @@ It wriggled and jiggled and tickled inside her.
 She swallowed the spider to catch the fly.
 I don't know why she swallowed the fly. Perhaps she'll die.
       LINES
+    when 3
+      <<-LINES
+I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
 
 end
