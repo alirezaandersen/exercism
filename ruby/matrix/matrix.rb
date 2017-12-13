15dbb7fc1fd134016fdffcc7f693aff6173171ef
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-13 14:17:19 -0700

    saddlepoint

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,9 +2,9 @@ class Matrix
 
   attr_reader :rows, :columns
 
-  def initialize(input)
+  def initalize(input)
     @rows = input.split("\n").map { |r| r.split(" ").map(&:to_i) }
     @columns = @rows.transpose
   end
-
+  
 end
