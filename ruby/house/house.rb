698f98e592a0b33cd4b6ae43df02e70bc4f7c188
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-17 10:17:08 -0700

    house

--- ruby/house/house.rb
+++ ruby/house/house.rb
@@ -1,3 +1,16 @@
 module House
-
+  @@parts = [
+    [""            ,  "horse and the hound and the horn"],
+    ["belonged to" ,  "farmer sowing his corn"],
+    ["kept"        ,  "rooster that crowed in the morn"],
+    ["woke"        ,  "priest all shaven and shorn"],
+    ["married"     ,  "man all tattered and torn"],
+    ["kissed"      ,  "maiden all forlorn"],
+    ["milked"      ,  "cow with the crumpled horn"],
+    ["tossed"      ,  "dog"],
+    ["worried"     ,  "cat"],
+    ["killed"      ,  "rat"],
+    ["ate"         ,  "malt"],
+    ["lay in"      ,  "house that Jack built."]
+  ]
 end
