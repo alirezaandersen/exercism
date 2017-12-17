89c4f6b2f8b87da3446a290843a289cbaacf762f
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-17 10:18:01 -0700

    house

--- ruby/house/house.rb
+++ ruby/house/house.rb
@@ -1,7 +1,7 @@
 module House
   def self.recite
-  "#{(1..@@parts.length).map { |i| verse(i) }.join("\n\n")}\n"
-end
+   "#{(1..@@parts.length).map { |i| verse(i) }.join("\n\n")}\n"
+  end
 
   @@parts = [
     [""            ,  "horse and the hound and the horn"],
