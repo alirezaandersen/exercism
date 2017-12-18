4247cb62e2cc10fffa3d2626bc4520d08ff61528
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-18 14:05:05 -0700

    secret-handshake

--- ruby/secret-handshake/secret_handshake_test.rb
+++ ruby/secret-handshake/secret_handshake_test.rb
@@ -20,7 +20,7 @@ class SecretHandshakeTest < Minitest::Test
   end
 
   def test_handshake_1000_to_jump
-    skip
+    # skip
     handshake = SecretHandshake.new(8)
     assert_equal ['jump'], handshake.commands
   end
