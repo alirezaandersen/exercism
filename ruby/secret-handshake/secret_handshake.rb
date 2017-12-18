4a26bfc3c6552e65f744179a6ec459ff4fbdcfb6
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-18 14:04:14 -0700

    secret-handshake

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -3,4 +3,11 @@ class SecretHandshake
   def initialize(flags)
     @flags = (flags.is_a? Integer) ? flags : 0
   end
+
+  @@items = {
+    'wink'            => 1,
+    'double blink'    => 2,
+    'close your eyes' => 4,
+    'jump'            => 8
+  }
 end
