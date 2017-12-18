346eecc7f715849b1d1ade9e181701627d8e66ee
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-18 14:04:20 -0700

    secret-handshake

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -4,6 +4,15 @@ class SecretHandshake
     @flags = (flags.is_a? Integer) ? flags : 0
   end
 
+  def commands
+    out = @@items.each_with_object([]) do |(k,v), acc|
+      binding.pry
+      acc.push(k) if (v & @flags) != 0
+    end
+    out.reverse! if (16 & @flags) != 0
+    out
+  end
+
   @@items = {
     'wink'            => 1,
     'double blink'    => 2,
