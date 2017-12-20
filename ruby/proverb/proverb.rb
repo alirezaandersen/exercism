a08d6e6df4c24fd0bf5b2dc253e70b744c9d9033
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-19 17:32:30 -0700

    proverb

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,4 +1,14 @@
 require 'pry'
 class Proverb
   @phrase = ""
-end 
+
+  def initialize(*items, qualifier: false)
+    @phrase = items.each_cons(2)
+    .map { |(i,j)| "For want of a #{i} the #{j} was lost." }
+    .join("\n")
+    want_of = (qualifier) ? "#{qualifier} #{items.first}" : "#{items.first}"
+    @phrase += ("\nAnd all for the want of a #{want_of}.")
+  end
+
+
+end
