051006220bccea063315fe6834962b9718138a25
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-21 10:38:43 -0700

    ocr

--- ruby/ocr-numbers/ocr_numbers.rb
+++ ruby/ocr-numbers/ocr_numbers.rb
@@ -6,6 +6,20 @@ class OcrNumbers
 
   def convert
     digits = []
+    @value.lines.each_slice(4).map do |row|
+      row = row.first(3)
+      row.map! { |r| r.tr("\n", " ") }
+      (0..row.first.length).step(3).map do |col|
+        parts = row.map { |part| part.slice(col, 3) }
+        if parts.any? { |p| p.strip != "" }
+          parts.map! { |o| (o.length == 2) ? "#{o} " : o }
+          parts.map! { |o| (o.length <  2) ? "   "   : o }
+          digits.push @@numbers.fetch(parts) { '?' }
+        end
+      end
+      digits.push(',')
+    end
+    digits.join.gsub(/,$/, '')
   end
 
   @@numbers = {
