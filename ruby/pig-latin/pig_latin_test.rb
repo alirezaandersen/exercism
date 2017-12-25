469ca02e6964448100fc0aef48a19b001917b8dc
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-25 11:27:42 -0700

    pig-latin

--- ruby/pig-latin/pig_latin_test.rb
+++ ruby/pig-latin/pig_latin_test.rb
@@ -109,7 +109,6 @@ class PigLatinTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 2, BookKeeping::VERSION
   end
 end
