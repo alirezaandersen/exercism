00a16d000e2bae39983db22718b6b3e2a2473a5d
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-27 10:22:44 -0700

    linked list

--- ruby/simple-linked-list/simple_linked_list_test.rb
+++ ruby/simple-linked-list/simple_linked_list_test.rb
@@ -157,7 +157,7 @@ class LinkedListTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
