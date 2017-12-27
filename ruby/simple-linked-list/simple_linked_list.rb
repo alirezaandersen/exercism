06b1b0d7ee5eccb6f6c1fbef3f351019d5e5c3f6
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-27 10:15:38 -0700

    linked list

--- ruby/simple-linked-list/simple_linked_list.rb
+++ ruby/simple-linked-list/simple_linked_list.rb
@@ -1,8 +1,30 @@
 class Element
 
+  attr_reader :datum
+  attr_accessor :next
 
+  def initialize(value)
+    @datum = value
+    @next = nil
+  end
 
 end
+
+class SimpleLinkedList
+
+  def initialize(array = nil)
+    create_from_array(array)
+  end
+
+  private
+
+  def create_from_array(array)
+    array.each { |value| push(Element.new(value)) }
+  end
+
+end
+
+
 
 
 module BookKeeping
