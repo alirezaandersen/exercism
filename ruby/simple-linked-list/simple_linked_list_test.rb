ddb0b7a098ad914fcdcd854b4bf8c54fbffd8cd9
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-27 10:23:43 -0700

    linked list

--- ruby/simple-linked-list/simple_linked_list_test.rb
+++ ruby/simple-linked-list/simple_linked_list_test.rb
@@ -8,19 +8,16 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_element_can_hold_a_different_value
-    # skip
     element = Element.new(10)
     assert_equal 10, element.datum
   end
 
   def test_element_next
-    # skip
     element = Element.new(1)
     assert_nil element.next
   end
 
   def test_element_next_can_be_assigned_to
-    # skip
     first  = Element.new(1)
     second = Element.new(2)
     first.next = second
@@ -28,14 +25,12 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_push
-    # skip
     list = SimpleLinkedList.new
     element = Element.new(1)
     assert_equal list, list.push(element)
   end
 
   def test_list_pop
-    # skip
     list = SimpleLinkedList.new
     element = Element.new(1)
     list.push(element)
@@ -43,13 +38,11 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_pop_empty
-    # skip
     list = SimpleLinkedList.new
     assert_nil list.pop
   end
 
   def test_list_pop_is_last_in_first_out
-    # skip
     list = SimpleLinkedList.new
     first = Element.new(1)
     second = Element.new(2)
@@ -58,13 +51,11 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_empty_to_array
-    # skip
     list = SimpleLinkedList.new
     assert_equal [], list.to_a
   end
 
   def test_list_single_to_array
-    # skip
     list = SimpleLinkedList.new
     first = Element.new(1)
     list.push(first)
@@ -72,7 +63,6 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_multiple_to_array
-    # skip
     list = SimpleLinkedList.new
     first = Element.new(1)
     second = Element.new(2)
@@ -82,21 +72,18 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_create_from_array
-    # skip
     array = [1, 2, 3]
     list = SimpleLinkedList.new(array)
     assert_equal [3, 2, 1], list.to_a
   end
 
   def test_list_created_from_array_still_made_up_of_elements
-    # skip
     array = [1, 2, 3]
     list = SimpleLinkedList.new(array)
     assert_equal Element, list.pop.class
   end
 
   def test_list_from_array_still_acts_as_lifo
-    # skip
     array = [1, 2, 3]
     list = SimpleLinkedList.new(array)
     element = list.pop
@@ -104,7 +91,6 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_in_place_reverse!
-    # skip
     first = Element.new(1)
     second = Element.new(2)
     third = Element.new(3)
@@ -115,7 +101,6 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_in_place_reverse_are_the_same_elements
-    # skip
     first = Element.new(1)
     second = Element.new(2)
     list = SimpleLinkedList.new
@@ -128,13 +113,11 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_list_reverse_empty_list
-    # skip
     list = SimpleLinkedList.new
     assert_equal list, list.reverse!
   end
 
   def test_works_for_1_through_10
-    # skip
     list = SimpleLinkedList.new(1..10)
     expected = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     assert_equal expected, list.to_a
@@ -157,7 +140,6 @@ class LinkedListTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    # skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
