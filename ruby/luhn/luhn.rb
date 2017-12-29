c2aba64cd192031411ce62e40b5b4ea0f96562df
Alireza Andersen <alireza.andersen@gmail.com>
2017-12-29 09:49:24 -0700

    luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,3 +1,16 @@
-class Luhn
 
+class Luhn
+  def self.valid?(string)
+    string.delete!(' ')
+    return false if string.size <= 1
+    return false if string =~ /\D/
+
+    digits_doubled = double_alternate_digits(string)
+    sum = digits_doubled.reduce(:+)
+    (sum % 10).zero?
+  end
+end
+
+module BookKeeping
+  VERSION = 1
 end
