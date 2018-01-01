a468ded739c36328d68fd12b1ae9ea10d44aaed9
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-01 12:45:48 -0700

    simple_cipher

--- ruby/simple-cipher/simple_cipher.rb
+++ ruby/simple-cipher/simple_cipher.rb
@@ -1,3 +1,15 @@
 class Cipher
+  attr_reader :key
 
-end 
+  def initialize(key = "aaaaaaaaaa")
+    validate_key(key)
+    @key = key
+  end
+
+
+  private
+
+  def validate_key(key)
+    raise ArgumentError unless key.match(/^[a-z]+$/)
+  end
+end
