000f81740897e45f409e71b0164e3aca14101b3c
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-02 15:25:31 -0700

    wordy

--- ruby/wordy/wordy.rb
+++ ruby/wordy/wordy.rb
@@ -16,4 +16,17 @@ class WordProblem
   def mathematic_parser
     question.scan(/\-?\d+|plus|minus|divided by|multiplied by/)
   end
+
+  def operators(index)
+    if  index.include?("-") || index =~ /\A\d+\z/
+      return index.to_i
+    end
+
+    case index
+    when 'plus' then :+
+    when 'minus' then :-
+    when 'multiplied by' then :*
+    when 'divided by' then :/
+    end
+  end
 end
