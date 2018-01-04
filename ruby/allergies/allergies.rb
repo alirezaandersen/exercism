ebbeb6f78e5aad9ce94311d3a7d11a43ce141e7b
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-04 09:15:45 -0700

    allergies

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,7 +1,5 @@
 class Allergies
 
-  # ALLERGENS = %w(eggs peanuts shellfish strawberries tomatoes chocolate pollen cats)
-
   ALLERGENS = %w(eggs peanuts shellfish strawberries tomatoes chocolate pollen cats)
 
   def initialize(score)
