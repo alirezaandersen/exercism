cc5939ef02d8ca4c2a08236cc903d19ea8022cf5
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-04 09:15:33 -0700

    allergies

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,5 +1,7 @@
 class Allergies
 
+  # ALLERGENS = %w(eggs peanuts shellfish strawberries tomatoes chocolate pollen cats)
+
   ALLERGENS = %w(eggs peanuts shellfish strawberries tomatoes chocolate pollen cats)
 
   def initialize(score)
@@ -7,10 +9,11 @@ class Allergies
   end
 
   def allergic_to?(item)
-    0 != @flags & 1 << ALLERGENS.find_index(item)
+    0 != @score & 1 << ALLERGENS.find_index(item)
   end
 
   def list
     ALLERGENS.select { |i| allergic_to?(i) }
   end
+
 end
