070e2bc75d529822ee486f8609509d1077594479
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-12 12:10:54 -0700

    allergies

--- ruby/allergies/allergies_test.rb
+++ ruby/allergies/allergies_test.rb
@@ -5,7 +5,6 @@ require 'minitest/pride'
 # Common test data version: 1.0.0 879bc89
 class AllergiesTest < Minitest::Test
   def test_no_allergies_means_not_allergic
-    # skip
     allergies = Allergies.new(0)
     refute allergies.allergic_to?('peanuts')
     refute allergies.allergic_to?('cats')
@@ -13,13 +12,11 @@ class AllergiesTest < Minitest::Test
   end
 
   def test_is_allergic_to_eggs
-    # skip
     allergies = Allergies.new(1)
     assert allergies.allergic_to?('eggs')
   end
 
   def test_allergic_to_eggs_in_addition_to_other_stuff
-    # skip
     allergies = Allergies.new(5)
     assert allergies.allergic_to?('eggs')
     assert allergies.allergic_to?('shellfish')
@@ -27,55 +24,46 @@ class AllergiesTest < Minitest::Test
   end
 
   def test_no_allergies_at_all
-    # skip
     allergies = Allergies.new(0)
     assert_equal %w(), allergies.list
   end
 
   def test_allergic_to_just_eggs
-    # skip
     allergies = Allergies.new(1)
     assert_equal %w(eggs), allergies.list
   end
 
   def test_allergic_to_just_peanuts
-    # skip
     allergies = Allergies.new(2)
     assert_equal %w(peanuts), allergies.list
   end
 
   def test_allergic_to_just_strawberries
-    # skip
     allergies = Allergies.new(8)
     assert_equal %w(strawberries), allergies.list
   end
 
   def test_allergic_to_eggs_and_peanuts
-    # skip
     allergies = Allergies.new(3)
     assert_equal %w(eggs peanuts), allergies.list
   end
 
   def test_allergic_to_more_than_eggs_but_not_peanuts
-    # skip
     allergies = Allergies.new(5)
     assert_equal %w(eggs shellfish), allergies.list
   end
 
   def test_allergic_to_lots_of_stuff
-    # skip
     allergies = Allergies.new(248)
     assert_equal %w(strawberries tomatoes chocolate pollen cats), allergies.list
   end
 
   def test_allergic_to_everything
-    # skip
     allergies = Allergies.new(255)
     assert_equal %w(eggs peanuts shellfish strawberries tomatoes chocolate pollen cats), allergies.list
   end
 
   def test_ignore_non_allergen_score_parts
-    # skip
     allergies = Allergies.new(509)
     assert_equal %w(eggs shellfish strawberries tomatoes chocolate pollen cats), allergies.list
   end
@@ -98,7 +86,6 @@ class AllergiesTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    # skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
