c605cf2f9f80cd6b887c20f55e446b53338bb11e
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-15 10:29:07 -0700

    poker

--- ruby/poker/poker.rb
+++ ruby/poker/poker.rb
@@ -13,8 +13,15 @@ end
 
 class Card
 
+  attr_reader :rank, :suit
+
+  def initialize(rank_and_suit)
+   @rank = rank_and_suit[0..-2]
+   @suit = rank_and_suit[-1]
+ end
+
   RANK_VALUES = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
                  '7' => 7, '8' => 8, '9' => 9, '10' => 10,
                  'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14}
-  
+
 end
