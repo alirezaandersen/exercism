df0f161d41852c483509335ee7ac9b3777dd167c
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-15 10:29:52 -0700

    poker

--- ruby/poker/poker.rb
+++ ruby/poker/poker.rb
@@ -24,4 +24,8 @@ class Card
                  '7' => 7, '8' => 8, '9' => 9, '10' => 10,
                  'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14}
 
+  def rank_values
+    RANK_VALUES[rank]
+  end
+  
 end
