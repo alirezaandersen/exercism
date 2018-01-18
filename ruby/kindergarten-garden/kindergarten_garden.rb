207c9698c185c2775b17e9e28d5fa094dd946553
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-18 15:03:29 -0700

    kinder

--- ruby/kindergarten-garden/kindergarten_garden.rb
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -10,10 +10,8 @@ class Garden
     %w(Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry)
   end
 
-
   private
-
-
+  
   def parse(diagram)
     diagram.split("\n").map do |row|
       row.split('').map do |sign|
@@ -21,7 +19,16 @@ class Garden
       end
     end
   end
-  
+
+  def plants
+    {
+      'G' => :grass,
+      'V' => :violets,
+      'R' => :radishes,
+      'C' => :clover
+    }
+  end
+
   def assign_pots
     students.each_with_index do |student, i|
       instance_eval "def #{student.downcase}; position = #{i}*2; pots[0][position,2] + pots[1][position,2]; end"
