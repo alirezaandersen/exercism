720d2405f850c249d7ee8bad9ef43eb279716654
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-18 14:59:32 -0700

    kinder

--- ruby/kindergarten-garden/kindergarten_garden.rb
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -10,4 +10,12 @@ class Garden
     %w(Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry)
   end
 
+
+  private
+
+  def assign_pots
+    students.each_with_index do |student, i|
+      instance_eval "def #{student.downcase}; position = #{i}*2; pots[0][position,2] + pots[1][position,2]; end"
+    end
+  end
 end
