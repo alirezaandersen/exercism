d0b082d0180098ff1fd15f6eea970b8f45b73d5f
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-24 17:08:59 -0700

    linked

--- ruby/linked-list/linked_list.rb
+++ ruby/linked-list/linked_list.rb
@@ -16,5 +16,9 @@ class Element
   attr_accessor :next
   attr_reader :datum
 
-  
+  def initialize(datum, next_element = nil, prev = nil)
+    @datum = datum
+    @next = next_element || self
+    @prev = prev || self
+  end
 end
