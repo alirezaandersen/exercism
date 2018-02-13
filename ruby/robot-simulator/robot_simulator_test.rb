3cc4a20d383dd0d73d5506f7214294ff1a830fde
Alireza Andersen <alireza.andersen@gmail.com>
2018-02-13 10:11:56 -0700

    robot-simulator

--- ruby/robot-simulator/robot_simulator_test.rb
+++ ruby/robot-simulator/robot_simulator_test.rb
@@ -59,7 +59,7 @@ class RobotTurningTest < Minitest::Test
   end
 
   def test_turn_left_from_east
-    skip
+    # skip
     robot.orient(:east)
     robot.turn_left
     assert_equal :north, robot.bearing
