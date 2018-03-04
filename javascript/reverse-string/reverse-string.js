a8f3520b6f36b098f14409f133361dd0b9c031b5
Alireza Andersen <alireza.andersen@gmail.com>
2018-03-04 16:52:47 -0700

    reverse-string

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,4 +1,11 @@
-var reverString(string){
-  string.split.reverse.join
+var reverseString = function(input){
 
+  if (input == '' || input == undefined) {
+
+  return '';
+
+  }else {
+  return input.split('').reverse().join('');
+  }
 }
+module.exports = reverseString;
