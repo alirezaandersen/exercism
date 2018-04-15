66adcaa659a9704d633ee80d8fbe01352af26f44
Alireza Andersen <alireza.andersen@gmail.com>
2018-04-14 23:22:25 -0700

    update

--- ruby/robot-simulator/robot_simulator.rb
+++ ruby/robot-simulator/robot_simulator.rb
@@ -16,17 +16,17 @@ class Robot
     self.bearing = direction
   end
 
-  # def advance
-  #  if bearing == :north
-  #    self.y += 1
-  #  elsif bearing == :south
-  #    self.y -= 1
-  #  elsif bearing == :west
-  #    self.x -= 1
-  #  else
-  #    self.x += 1
-  #  end
-  # end
+  def advance
+   if bearing == :north
+     self.y += 1
+   elsif bearing == :south
+     self.y -= 1
+   elsif bearing == :west
+     self.x -= 1
+   else
+     self.x += 1
+   end
+  end
 
  def turn_right
    turn(:+)
