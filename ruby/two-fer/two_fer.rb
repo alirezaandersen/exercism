43f8c040993db62f1092ee459d5e74b4dbb82a77
Alireza Andersen <alireza.andersen@gmail.com>
2018-08-08 11:58:05 -0600

    working method

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -2,8 +2,13 @@ require 'pry'
 class TwoFer
 
   def self.two_fer(name = nil)
-
+    if name != nil
+      "One for #{name}, one for me."
+    else
+      "One for you, one for me."
+    end
   end
+  
 end
 
 
